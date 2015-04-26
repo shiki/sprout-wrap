61144a45aee48da1196ec36ff1f9cb72e97021d7
Shiki <jayson@basanes.net>
2015-04-26 12:58:17 -0600

    PHP extensions

--- site-cookbooks/php/recipes/install.rb
+++ site-cookbooks/php/recipes/install.rb
@@ -7,3 +7,14 @@ homebrew_tap 'homebrew/homebrew-php'
 package 'php56' do 
   options '--with-imap --with-mssql --with-postgresql --with-tidy'
 end
+
+package 'php56-geoip'
+package 'php56-gmagick'
+package 'php56-http'
+package 'php56-imagick'
+package 'php56-mcrypt'
+package 'php56-memcache'
+package 'php56-memcached'
+package 'php56-mongo'
+package 'php56-xdebug'
+
